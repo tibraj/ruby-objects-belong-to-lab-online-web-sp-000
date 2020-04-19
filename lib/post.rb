1c2a990bad114a6496548fb3bed6762ef33dc18f
Taulant Ibraj <tibraj94@gmail.com>
2020-04-19 18:33:15 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,18 +1,17 @@
 class Post
-<<<<<<< HEAD
+
   attr_accessor :title, :author
-=======
+
   attr_accessor :title
->>>>>>> e338af458b3e22d4330640c55d83066d0512bbaa
+
 
   def initialize
     @name = title
   end
-<<<<<<< HEAD
-=======
+
 
   def author=(post)
     post.author = author
   end
->>>>>>> e338af458b3e22d4330640c55d83066d0512bbaa
+
 end
