4c7457ab4a3ab86c876c2de17f637b69eff1ddf5
Taulant Ibraj <tibraj94@gmail.com>
2020-04-19 18:33:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,10 @@
 class Song
-<<<<<<< HEAD
   attr_accessor :title, :artist
 
   def initialize
     @title = title
   end
-=======
+
   attr_accessor :title
 
   def initialize(title)
@@ -13,5 +12,4 @@ class Song
   end
 
 
->>>>>>> e338af458b3e22d4330640c55d83066d0512bbaa
 end
